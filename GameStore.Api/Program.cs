5b8cb49a470594c02c99a749d31b6f8d47f7c414
Soumik Sen <mastersoumik2003@gmail.com>
2026-03-25 23:14:46 +0530

    Pushing DB migration updates

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddValidation();
 
 var connString = "Data Source=GameStore.db";
+
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
 var app = builder.Build();
